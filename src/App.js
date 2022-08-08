e2554e87d6f8d7a1c79c81a35bad734b0a7e141b
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-08 18:18:37 +0200

    Add export to pdf function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Contacts } from './components/Contacts'
 import { Education } from './components/Education'
 import { Skills } from './components/Skills'
 import { Experience } from './components/Experience'
+import ReactToPrint from 'react-to-print';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className="grid-app">
+      <div className="grid-app" ref={el => (this.componentRef = el)}>
         <div className="grid-left">
           <Photo />
           <Contacts />
@@ -21,6 +23,14 @@ class App extends React.Component {
           <Identity />
           <Education />
           <Experience />
+        </div>
+        <div className="print-button no-print flex justify-center">
+          <ReactToPrint
+            trigger={() => {
+              return <button>Generate PDF</button>
+            }}
+            content={() => this.componentRef}
+          />
         </div>
       </div>
     )
